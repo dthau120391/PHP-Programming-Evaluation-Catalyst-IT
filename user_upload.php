1c7d37e0bdc01c40883fc0bb25f6fcd8922e995c
Lucas Hau Dai <dthau120391@gmail.com>
2017-07-19 21:29:26 +1000

    Get directives from commandline

--- user_upload.php
+++ user_upload.php
@@ -8,7 +8,46 @@
  */
 
 
+function getDirectives($args)
+{
+	//Skip file name
+	array_shift($args);
+	$args = join( $args, ' ' );
+    	
+	//
+    	preg_match_all('/ (--\w+ (?:[= ] [^-]+ [^\s-] )? ) | (-\w+) | (\w+) /x', $args, $match );
+	var_dump($match);die;
+    	$args = array_shift( $match );
 
+    	$directives = array(
+        	'input'    => array(),
+        	'commands' => array(),
+        	'flags'    => array()
+    	);
 
-echo "\n";
+    	foreach ($args as $arg)
+	{
+        	//Is it a command? (prefixed with "--")
+        	if (substr($arg, 0, 2) === '--') 
+		{
+		    	$value = preg_split('/[= ]/', $arg, 2 );
+		    	$com   = substr(array_shift($value), 2 );
+		    	$value = join($value);
+
+		    	$ret['commands'][$com] = !empty($value) ? $value : true;
+		    	continue;
+        	}
+
+        	//Is it a flag? (prefixed with "-")
+        	if (substr($arg, 0, 1) === '-') 
+		{
+		    	$ret['flags'][] = substr($arg, 1);
+		    	continue;
+        	}
+
+        	$ret['input'][] = $arg;
+        	continue;
+    	}
+    	return $directives;
+}
 ?>
