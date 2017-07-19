40a2f80f193123e676e0fc60997016a30d172f62
Lucas Hau Dai <dthau120391@gmail.com>
2017-07-19 20:29:32 +1000

    Init Documents

---
+++ foobar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Output the numbers from 1 to 100
+ * Where the number is divisible by three (3) output the word “foo”
+ * Where the number is divisible by five (5) output the word “bar”
+ * Where the number is divisible by three (3) and (5) output the word “foobar”
+ *
+ * @author     dthau120391@gmail.com
+ * @version    1.0
+ */
+
+
+
+
+echo "\n";
+?>
---
+++ user_upload.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Process a CSV file as an input and parse file data is to be
+ * inserted into a MySQL database
+ *
+ * @author     dthau120391@gmail.com
+ * @version    1.0
+ */
+
+
+
+
+echo "\n";
+?>
